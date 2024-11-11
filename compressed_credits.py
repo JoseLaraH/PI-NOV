52dae8bde7f68c1449aad87687d215f8a196ca25
JoseLaraH <carlos.l4@hotmail.com>
2024-11-11 00:28:28 -0500

    Clear some information

--- compressed_credits.py
+++ compressed_credits.py
@@ -6,5 +6,5 @@ credits_df = pd.read_csv('DATA/credits.csv')
 # Seleccionar una muestra aleatoria del 25% de los datos
 sampled_df = credits_df.sample(frac=0.25, random_state=42)
 
-# Guardar el archivo reducido en la misma carpeta con el nombre "credits_small.csv"
-sampled_df.to_csv('final_data/credits_small.csv', index=False)
+# Guardar el archivo reducido en la misma carpeta
+sampled_df.to_csv('DATA/credits_small.csv', index=False)
